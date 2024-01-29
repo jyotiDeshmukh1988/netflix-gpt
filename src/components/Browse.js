70746fd6f5751b20a40b157604fc4778b7017417
Jyoti Deshmukh <test8600896492@gmail.com>
2024-01-29 19:46:17 +0530

    create moviesSlice and store the TBDB api data into redux store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import {addNowPlayingMovies} from "../utils/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
   const getNowPlayingMovies = async() => {
-    const results = await fetch("https://api.themoviedb.org/3/movie/now_playing&page=1",API_OPTIONS);
+    const results = await fetch("https://api.themoviedb.org/3/movie/now_playing?page=1",API_OPTIONS);
     const data = await results.json();
-    console.log(data);
+    console.log(data.results);
+    dispatch(addNowPlayingMovies(data.results));
   };
   useEffect(() => {
     getNowPlayingMovies();
