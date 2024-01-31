d284637f05bea208124c85ce342ed6b7c74118da
Jyoti Deshmukh <test8600896492@gmail.com>
2024-01-31 15:25:31 +0530

    add trailer video to redux store

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,8 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 const VideoBackground = ({ movieId }) => {
-  const [trailerId,setTrailerId] = useState(null);
-  // fetch trailer video
+  const dispatch = useDispatch();
+  const trailerVideo = useSelector((store)=>{
+      return store.movies?.trailerVideo;
+  })
+  // fetch trailer video and updating the store
   const getMovieVideos = async () => {
     const data = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
@@ -15,19 +20,21 @@ const VideoBackground = ({ movieId }) => {
       return video.type === "Trailer";
     });
     const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    setTrailerId(trailer.key);
+    //console.log(trailer);
+    dispatch(addTrailerVideo(trailer));
   };
   useEffect(() => {
     getMovieVideos();
   }, []);
   return (
     <div>
-      {/*To display the trailor ID dynamic in the iframe video will use the state variable this is one solution*/}
+      {/*To display the trailor ID dynamic in the iframe video 
+      will use the redux store to store the trailer ID in 
+      the movieSlice this is second solution*/}
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/"+trailerId+"?si=6Eb3QI1gyTAb9h-K"}
+        src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?si=6Eb3QI1gyTAb9h-K"}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
