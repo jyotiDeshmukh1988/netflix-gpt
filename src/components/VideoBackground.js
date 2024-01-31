e3ca58baeb75817fc400cf5b46c695cc8562af01
Jyoti Deshmukh <test8600896492@gmail.com>
2024-01-31 15:37:10 +0530

    create custom hook for the video and use the hook in the Videobackground component

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,31 +1,14 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailor from "../hooks/useMovieTrailer";
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
-  const trailerVideo = useSelector((store)=>{
-      return store.movies?.trailerVideo;
-  })
-  // fetch trailer video and updating the store
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    );
-    const json = await data.json();
-    //console.log(json);
+  const trailerVideo = useSelector((store) => {
+    return store.movies?.trailerVideo;
+  });
 
-    const filterData = json.results.filter((video) => {
-      return video.type === "Trailer";
-    });
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    //console.log(trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+  // create custom hook for getting the trailer video from the redux store
+  useMovieTrailor(movieId);
+
   return (
     <div>
       {/*To display the trailor ID dynamic in the iframe video 
@@ -34,7 +17,11 @@ const VideoBackground = ({ movieId }) => {
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?si=6Eb3QI1gyTAb9h-K"}
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?si=6Eb3QI1gyTAb9h-K"
+        }
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
