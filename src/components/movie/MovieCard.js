d75fd7ce84f0309c23808acff1c997217547945a
Jyoti Deshmukh <test8600896492@gmail.com>
2024-02-06 15:24:00 +0530

    add modal popup for movie card

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { IMG_CDN_URL } from '../../utils/constants'
+import Modal from '../Modal';
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({posterPath,title,desc}) => {
+  const [open, setOpen] = React.useState(false);
+ 
+    const handleClose = () => {
+        setOpen(false);
+    };
+ 
+    const handleOpen = () => {
+        setOpen(true);
+    };
     if(!posterPath) return;
   return (
-    <div className="w-48 md:w-55 pr-6 cursor-pointer hover:scale-110"><img className="rounded-lg" alt="Movie Card" src={IMG_CDN_URL+posterPath}/></div>
+    <>
+    <div className="w-48 md:w-55 pr-6 cursor-pointer"><img onClick={handleOpen} className="rounded-lg" alt="Movie Card" src={IMG_CDN_URL+posterPath}/></div>
+    <Modal isOpen={open} onClose={handleClose}><h1 className='text-2xl font-bold mb-2 text-center'>{title}</h1><img className="rounded-lg" alt="Movie Card" src={IMG_CDN_URL+posterPath}/><p className='mt-2 text-sm'>{desc}</p></Modal>
+    </>
   )
 }
 
--- src/components/movie/MovieList.js
+++ src/components/movie/MovieList.js
@@ -2,14 +2,14 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, mymovies }) => {
-  //console.log(mymovies);
+  console.log(mymovies);
   return (
     <div className="py-6 bg-black px-6">
       {title && <h1 className="text-2xl py-3 text-white">{title}</h1>}
       <div className="flex overflow-x-scroll container-snap overflow-y-hidden">
         <div className="flex ">
           {mymovies?.map((movie) => {
-            return <MovieCard key={movie?.id} posterPath={movie?.poster_path} />;
+            return <MovieCard key={movie?.id} posterPath={movie?.poster_path} title={movie?.original_title} desc={movie?.overview}/>;
           })}
         </div>
       </div>
